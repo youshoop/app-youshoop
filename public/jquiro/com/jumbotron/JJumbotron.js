4a385c9edbc6b955f7554dba8c57d18d636cbcb2
youshoop <youshoop@outlook.com>
2014-05-09 09:15:38 -0500

    files new de jqurio

---
+++ public/jquiro/com/jumbotron/JJumbotron.js
@@ -0,0 +1,39 @@
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+define(["require", "exports", '.././htmlObject/HtmlObject'], function(require, exports, HtmlObject) {
+    /**
+    @ Autor : Yonatan Alexis Quintero Rodriguez
+    @ Version: 0.1
+    @ Fecha : 22 - 03 - 2014
+    * Elemento progress de jumbotron bootstrap (http://getbootstrap.com/components/#jumbotron)
+    **/
+    var JJumbotron = (function (_super) {
+        __extends(JJumbotron, _super);
+        function JJumbotron() {
+            _super.call(this, 'jjumbotron');
+
+            this.addClass('jumbotron');
+        }
+        JJumbotron.prototype.init = function () {
+        };
+
+        JJumbotron.prototype.create = function (_id) {
+            _id.appendChild(this.getHtml());
+            _id = null;
+        };
+
+        JJumbotron.prototype.getHtml = function () {
+            return this.toHtml();
+        };
+
+        JJumbotron.prototype.finalize = function () {
+            this.expire();
+        };
+        return JJumbotron;
+    })(HtmlObject.div);
+    exports.JJumbotron = JJumbotron;
+});
